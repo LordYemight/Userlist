175e9209e5e1147b9f83ade98c94c7e96942beaf
Okunola Olayemi <Okunola.O@bullseye.local>
2023-05-22 00:49:15 +0100

    Deployed website

---
+++ src/UserList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './UserList.css';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="user-list-container">
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
